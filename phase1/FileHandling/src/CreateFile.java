0757bdd49dcbded4c4346d6c68832df90dc7afd6
github_Saikumar8892 <sairoyalsulli@gmail.com>
2022-09-17 09:42:39 +0530

    changes

---
+++ phase1/FileHandling/src/CreateFile.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+public class CreateFile {
+
+	public static void main(String[] args) {
+		try {
+			File myObj = new File("H:\\Sai.txt");
+			if (myObj.createNewFile()) {
+				System.out.println("File created");
+			}else {
+				System.out.println("File already exixts");
+			}
+		}catch(Exception e){
+			System.out.println("Error occured");
+			e.printStackTrace();
+	  }
+	}
+}
---
+++ phase1/FileHandling/src/WriteFile.java
@@ -0,0 +1,17 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFile {
+
+	public static void main(String[] args) {
+		try {
+			FileWriter myWriter = new FileWriter("H:\\Sai.txt");
+			myWriter.write("Welcome to simplilearn");
+			myWriter.close();
+			System.out.println("updated data sucessfully");
+		} catch (IOException e) {
+			System.out.println("error");
+			e.printStackTrace();
+		}
+	}
+}
